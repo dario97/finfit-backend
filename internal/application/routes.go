8acdf98dc38caf04a455597eaf90434e58b1726a
Dario Marquez <ext_darmarqu@mercadolibre.com>
2022-12-29 15:37:15 -0300

    changed path

--- internal/application/routes.go
+++ internal/application/routes.go
@@ -4,5 +4,5 @@ import "github.com/labstack/echo/v4"
 
 func mapRoutes(e *echo.Echo) {
 	e.POST("/expense", ExpenseHandler.Add)
-	e.GET("/expense", ExpenseHandler.SearchInPeriod)
+	e.GET("/expense/search", ExpenseHandler.SearchInPeriod)
 }
